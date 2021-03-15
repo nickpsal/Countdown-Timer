5e37c7cedda08cfd5caa05814b827612c0a9aac1
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2021-03-15 17:10:02 +0200

    commit 15/03/2021 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,69 @@
 function startTimer() {
-    var day = $("#day");
+    var day = $("#day").val();
     var month = document.getElementById("months").value;
     var year = document.getElementById("year").value;
-    var hour = $("#hour");
-    var min = $("#min");
+    var hour = $("#hour").val();
+    var min = $("#min").val();
     var sec = 00;
-    var date = year + "-" + month + "-" + day.val();
-    var time = hour.val() + ":" + min.val() + ":" + sec;
-    var val = moment(date);
-    if (!val.isValid() || hour.val() > 23 || hour.val() < 0 || min.val() > 59 || min.val() < 0) {
-        alert("Λαθος Ημερομηνία");
+    if (!validateDate(day,month)) {
+        alert("Δεν δώσατε σωστή Ημερομηνία");
     }else {
-        date = month + " " + day.val() + "," + year;
-        countdownStart(date,time)
+        if (!validateTime(hour,min)){
+            alert("Δεν δώσατε σωστή ώρα");
+        }else{
+            var time = hour + ":" + min + ":" + sec;
+            var date = new Date(month + " " + day + ", " + year + " " + time).getTime();
+            countdownStart(date);
+        }
+    }    
+}
+
+function validateDate(day,month){
+    if (month == "Feb" && (day > 28 || day <1)){
+        return false;
+    }else if (month == "Jan" || month == "Mar" || month == "May" || month == "Jul" || month == "Aug" || month == "Oct" || month == "Dec") {
+        if (day < 1 || day >31) {
+            return false;
+        }
+    }else if (month == "Apr" || month == "Jun" || month == "Sep" || month == "Nov") {
+        if (day < 1 || day >30) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function validateTime(hour,min ){
+    if (hour < 0 || hour > 23 || min <0 || min > 60) {
+        return false;
+    }else{
+        return true;
     }
 }
 
-function countdownStart(date,time) {
+function countdownStart(date) {
     clearInterval(x);
-    var countDown = new Date(date + " " + time).getTime();
-    var y = new Date(date + " " + time).getFullYear();
     var x = setInterval(function() {
         var now = new Date().getTime();
-        var nowYear = new Date().getFullYear();
-        year = y -nowYear;
-        count = countDown - now;
-        var days = Math.floor(count/(1000*60*60*24));
-        var hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((count % (1000 * 60)) / 1000);
-        if (year == 0) {
-            document.getElementById('countdown').innerHTML = days + " Μέρες και " + hours + ":" + minutes + ":" + seconds + " s ";
-        }else if (days == 0) {
-            document.getElementById('countdown').innerHTML = hours + ":" + minutes + ":" + seconds + " s ";
-        }else if (days == 0 && hours == 0) {
-            document.getElementById('countdown').innerHTML = minutes + ":" + seconds + " s ";
-        }else if (days == 0 && hours == 0 && minutes == 0) {
-            document.getElementById('countdown').innerHTML = seconds + " s ";
-        }else{
-            document.getElementById('countdown').innerHTML = year + " Χρόνια " + days + " Μέρες και " + hours + ":" + minutes + ":" + seconds + " s ";
+        var dif = date - now;
+        var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+        var years = Math.floor(days % (365));
+        var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((dif % (1000 * 60)) / 1000);
+        if (years == 0 && days == 0 && hours == 0 && minutes == 0) {
+            document.getElementById("countdown").innerHTML = seconds;
+        }else if (years == 0 && days == 0 && hours == 0) {
+            document.getElementById("countdown").innerHTML = minutes + ":" + seconds;
+        }else if (years == 0 && days == 0) {
+            document.getElementById("countdown").innerHTML = hours + ":" + minutes + ":" + seconds;
+        }else if (years == 0) {
+            document.getElementById("countdown").innerHTML = days + " Μέρες " + hours + ":" + minutes + ":" + seconds;
+        }else {
+            document.getElementById("countdown").innerHTML = years + " Χρόνια " +  days + " Μέρες " + hours + ":" + minutes + ":" + seconds;
         }
-        if (count < 0 ){
+        if (dif < 0) {
             clearInterval(x);
-            document.getElementById("countdown").innerHTML = "Τελος Χρόνου";
             alert("Τέλος Χρόνου");
         }
     },1000);
